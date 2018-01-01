83853ab4cf227fd51e71435ce68a910f2cc2b1cf
aavzz <aavzz@yandex.ru>
2018-01-01 21:22:03 +0400

    Update syslog.go

--- setup/syslog/syslog.go
+++ setup/syslog/syslog.go
@@ -3,12 +3,6 @@ Package syslog sets up logging to local syslog
 */
 package syslog
 
-/*
- * this code runs both in parent and child
- * parent's output goes to stdout,
- * child's to /dev/null
- */
-
 import (
 	"fmt"
 	"log/syslog"
@@ -21,6 +15,9 @@ var SysLog *syslog.Writer
 // InitLogging tries to get in touch with local syslog.
 // It writes error message to stdout and stops the
 // process in case of failure.
+// It runs both in parent and child
+// parent's output goes to stdout,
+// child's to /dev/null
 func InitLogging() {
 	var err error
 	SysLog, err = syslog.New(syslog.LOG_INFO|syslog.LOG_DAEMON, "stubd")
