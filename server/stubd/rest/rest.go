020debb4898b77cd06653ffd1e43cc69ce1971f0
aavzz <aavzz@yandex.ru>
2018-01-05 20:42:48 +0400

    Update rest.go

--- server/stubd/rest/rest.go
+++ server/stubd/rest/rest.go
@@ -6,6 +6,7 @@ package rest
 import (
 	"context"
 	"github.com/aavzz/stub-server/server/common/log"
+	"github.com/aavzz/stub-server/server/common/pid"
 	"github.com/aavzz/stub-server/server/common/signal"
 	"github.com/aavzz/stub-server/server/stubd/rest/api1"
 	"github.com/gorilla/mux"
@@ -30,6 +31,7 @@ func InitHttp() {
 			ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 			log.Info("SIGQUIT received, exitting gracefully")
 			s.Shutdown(ctx)
+			pid.Remove()
 		})
 	}
 
